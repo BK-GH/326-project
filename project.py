d240ce316892717c69b57753a2691ba96941c6f1
GeraltofReact <65792094+GeraltofReact@users.noreply.github.com>
2020-10-21 16:38:09 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,46 @@
+"""
+Project: Assignment Tracker
+Brook Goitom | Emmanuel Sitaniapessy | Jenny Dang | Nicholas Wang
+INST 326
+Professor Cruz
+"""
+
+class Assignment:
+    
+    def due_date():
+        pass
+    
+    def name():
+        pass
+    
+    def course_name():
+        pass
+    
+    def weight():
+        pass
+
+
+class Status:
+    
+    def days_left():
+        pass
+    
+    def reminder():
+        pass
+    
+    def countdown():
+        pass
+    
+    def organize_list():
+        pass
+    
+    def estimate():
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ =="__main__":
+    pass
